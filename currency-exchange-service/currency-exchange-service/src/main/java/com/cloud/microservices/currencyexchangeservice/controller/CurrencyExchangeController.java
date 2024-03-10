7bc734d0a5e1bf6128850d7769ace63215f925e0
soumin mohanty <soummohanty@deloitte.com>
2024-03-10 18:17:52 +0530

    added logger

--- currency-exchange-service/currency-exchange-service/src/main/java/com/cloud/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/cloud/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.cloud.microservices.currencyexchangeservice.controller;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,8 @@ import com.cloud.microservices.currencyexchangeservice.repository.ExchangeServic
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private ExchangeServiceRepository exchangeServiceRepo;
 	
@@ -24,6 +26,7 @@ public class CurrencyExchangeController {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		logger.info("retrieveExchangeValue called with {} to {}",from,to);
 //		CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
 		CurrencyExchange currencyExchange = exchangeServiceRepo.findByFromAndTo(from, to);
 		if(currencyExchange == null) {
